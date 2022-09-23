7cf4480a9281ddd357bfdce73848a24a0008d5fb
Igor Lima <igorlimagn@gmail.com>
2022-09-23 20:44:34 -0300

    @igorgll [Creating PI]

---
+++ Quiz.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Quiz {
+
+    static Scanner input = new Scanner(System.in);
+
+    static int menu() {
+        int opcao;
+
+        do {
+            System.out.println("1- INTRUÇÕES");
+            System.out.println("2- JOGAR");
+            System.out.println("3- CRÉDITOS");
+            System.out.println("4- SAIR");
+
+            System.out.println("DIGITE A OPÇÃO DESEJADA");
+            opcao = input.nextInt();
+
+            switch(opcao) {
+                case 1: System.out.println("BEM VINDO AS INSTRUÇÕES DO QUIZ");
+                break;
+
+                case 2: System.out.println("BEM VINDO AO JOGO!");
+                break;
+
+                case 3: System.out.println("BEM VINDO AOS CRÉDITOS...");
+                break;
+
+                case 4: System.out.println("SAINDO...");
+                break;
+            }
+
+            if (opcao > 4 || opcao < 1) {
+                System.out.println("OPÇÃO INVÁLIDA");
+            }
+
+        } while (opcao > 4 || opcao < 1);
+
+        return opcao;
+    }
+
+    public static void main(String[] args) {
+        int opcaoEscolhida;
+
+        System.out.println("Seja Bem Vindo(a) ao Quiz!");
+
+        do {
+            opcaoEscolhida = menu();
+        } while (opcaoEscolhida == 1 || opcaoEscolhida == 2 || opcaoEscolhida == 3 || opcaoEscolhida == 4);
+    }
+
+}
