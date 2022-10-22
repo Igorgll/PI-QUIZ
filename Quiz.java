ff965876e9dcb24b6a1fe54eeb65af9aff7420d5
Igor Lima <igorlimagn@gmail.com>
2022-10-21 21:35:49 -0300

    @igorgll [Perguntas]

--- Quiz.java
+++ Quiz.java
@@ -71,17 +71,30 @@ public class Quiz {
         pergunta1();
     }
 
+    public static String getPerguntas(int indice) {
+        String perguntas[] = new String[10];
+        // VETOR DE PERGUNTAS
+        perguntas[0] = "1- QUEM FOI O MAIOR ARTILHEIRO DAS COPAS DO MUNDO ATÉ O MOMENTO?";
+        perguntas[1] = "2- QUEM SÃO OS MAIORES CAMPEÕES DA COPA DO MUNDO?";
+        perguntas[2] = "3- QUANDO FOI REALIZADO A ÚLTIMA COPA DO MUNDO ATÉ O MOMENTO?";
+        perguntas[3] = "4- QUEM FOI O JOGADOR MAIS CONVOCADO PARA AS COPAS DO MUNDO ATÉ O MOMENTO?";
+        perguntas[4] = "5- QUAL É O ÚNICO JOGADOR QUE JÁ PARTICIPOU DE 3 FINAIS DA COPA DO MUNDO SEGUIDAS?";
+        perguntas[5] = "6- QUAL PAÍS GANHOU A PRIMEIRA COPA DO MUNDO?";
+        perguntas[6] = "7- QUAL O JOGADOR COM MAIS TÍTULOS DA COPA DO MUNDO?";
+        perguntas[7] = "8- QUAL A SELEÇÃO QUE MARCOU O GOL MAIS RÁPIDO NUMA COPA DO MUNDO?";
+        perguntas[8] = "9- QUANDO ACONTECEU A PRIMEIRA COPA DO MUNDO NO BRASIL?";
+        perguntas[9] = "10- EM QUAL ESTÁDIO FOI A ABERTURA DA COPA DO MUNDO EM 2014?";
+        String retorno = perguntas[indice];
+        return retorno;
+    }
+
     // MÉTODO SEPARADO PARA PERGUNTA NÚMERO 1
     public static void pergunta1() {
         String resposta;
         boolean acerto = false;
         int tentativas = 0;
-        String perguntas[] = new String[10];
-
-        // VETOR DE PERGUNTAS
-        perguntas[0] = "1- QUEM FOI O MAIOR ARTILHEIRO DAS COPAS DO MUNDO ATÉ O MOMENTO?";
-
         List<String> alternativas = new ArrayList<String>();
+
         alternativas.add("Miroslav Klose"); // RESPOSTA CORRETA
         alternativas.add("Ronaldo");
         alternativas.add("Gerd Müller");
@@ -90,7 +103,7 @@ public class Quiz {
         do {
             Collections.shuffle(alternativas);
             System.out.println();
-            System.out.println(perguntas[0]);
+            System.out.println(getPerguntas(1));
             System.out.println();
             System.out.println("a) " + alternativas.get(0)); // RESPOSTA CORRETA
             System.out.println("b) " + alternativas.get(1));
